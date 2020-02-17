37255c251abde911482cc8e64f835a1e72c10b44
Vladislav <i.96petrov@gmail.com>
2020-02-17 13:10:07 +0300

    some commands done

---
+++ src/commands/Filter_starts_with_name.java
@@ -0,0 +1,12 @@
+package commands;
+
+public class Filter_starts_with_name implements ICommand {
+    public Filter_starts_with_name(Commands com){
+        com.addNewCommand("filter_starts_with_name", this);
+    }
+
+    @Override
+    public void Do(String parameter1, String parameter2){
+
+    }
+}
--- src/movie/MovieCollection.java
+++ src/movie/MovieCollection.java
@@ -3,14 +3,25 @@ import java.util.*;
 
 public class MovieCollection {
     private static Map<Long, Movie> Movies = new LinkedHashMap<Long, Movie>();
+
     public static void addMovie(Long id, String movieName){
         Movie movie = new Movie(id, movieName);
         Movies.put(id, movie);
     }
+
     public static void removeMovie(Long id){
         Movies.remove(id);
     }
+
     public static void clearMovies(){
         Movies.clear();
+    }
+
+    public static Set<Long> getKeySet() {
+        return Movies.keySet();
+    }
+
+    public static Movie getValue(Long key) {
+        return Movies.get(key);
     }
 }
