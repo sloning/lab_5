753f2826081113c4fb2b2720f155f861f7fa6839
takov-abay <61031325+takov-abay@users.noreply.github.com>
2020-05-22 20:05:07 +0300

    подкорректировал добавление фильмов (теперь нельзя добавлять фильмы по одинаковому ключу)

--- server/src/main/java/commands/Insert_key.java
+++ server/src/main/java/commands/Insert_key.java
@@ -45,38 +45,47 @@ public class Insert_key implements ICommand {
     public String Do(String parameter, Movie movie, String user) throws IOException {
         try {
             Statement statement = DBWorker.getConnection().createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
-            String CoordinatesQuery = "insert into coords(x,y) values (" + movie.getCoordinatesX() + ", " + movie.getCoordinatesY() + ")";
-            statement.executeUpdate(CoordinatesQuery);
-            ResultSet resultCoordinatesSet = statement.executeQuery("select coords_id from coords");
-            resultCoordinatesSet.afterLast(); //переводит курсор на последний добавленный элемент
-            resultCoordinatesSet.previous();
-            String CoordinatesId = resultCoordinatesSet.getString(1);
-            String LocationQuery = "insert into locations(x,y,z,location_name) values (" + movie.getDirectorLocationX() + ", " + movie.getDirectorLocationY() + ", " + movie.getDirectorLocationZ() + ", '" + movie.getDirectorLocationName() + "')";
-            statement.executeUpdate(LocationQuery);
-            ResultSet resultLocationSet = statement.executeQuery("select location_id from locations");
-            resultLocationSet.afterLast();
-            resultLocationSet.previous();
-            String LocationId = resultLocationSet.getString(1);
-            String DirectorQuery = "insert into directors(director_name, birthday, height, weight, location) values ('" + movie.getDirectorName() + "', '" + movie.getDirectorBirthday() + "', " + movie.getDirectorHeight() + ", " + movie.getDirectorWeight() + ", '" + LocationId + "')";
-            statement.executeUpdate(DirectorQuery);
-            ResultSet resultDirectorSet = statement.executeQuery("select director_id from directors");
-            resultDirectorSet.afterLast();
-            resultDirectorSet.previous();
-            String DirectorId = resultDirectorSet.getString(1);
-            String MovieQuery = "insert into movies(movie_name, movie_coords, date_of_creation, oscars, length, movie_genre, movie_rating, director, movie_key, usernames) values ('"
-                    + movie.getName() + "', '" + CoordinatesId + "', '" + movie.getCreationDate() + "', " + movie.getOscars() + ", " + movie.getLength() + ", '" + movie.getGenre().toString() + "', '"
-                    + movie.getMpaaRating().toString() + "', '" + DirectorId + "', '" + parameter + "', '" + user + "')";
-            statement. executeUpdate(MovieQuery);
+            ResultSet resultCheckSet = statement.executeQuery("select movie_key from movies where movie_key = '" + parameter + "'");
+            boolean f = true;
+            while (resultCheckSet.next()){
+                f = false;
+            }
+            if (f) {
+                String CoordinatesQuery = "insert into coords(x,y) values (" + movie.getCoordinatesX() + ", " + movie.getCoordinatesY() + ")";
+                statement.executeUpdate(CoordinatesQuery);
+                ResultSet resultCoordinatesSet = statement.executeQuery("select coords_id from coords");
+                resultCoordinatesSet.afterLast(); //переводит курсор на последний добавленный элемент
+                resultCoordinatesSet.previous();
+                String CoordinatesId = resultCoordinatesSet.getString(1);
+                String LocationQuery = "insert into locations(x,y,z,location_name) values (" + movie.getDirectorLocationX() + ", " + movie.getDirectorLocationY() + ", " + movie.getDirectorLocationZ() + ", '" + movie.getDirectorLocationName() + "')";
+                statement.executeUpdate(LocationQuery);
+                ResultSet resultLocationSet = statement.executeQuery("select location_id from locations");
+                resultLocationSet.afterLast();
+                resultLocationSet.previous();
+                String LocationId = resultLocationSet.getString(1);
+                String DirectorQuery = "insert into directors(director_name, birthday, height, weight, location) values ('" + movie.getDirectorName() + "', '" + movie.getDirectorBirthday() + "', " + movie.getDirectorHeight() + ", " + movie.getDirectorWeight() + ", '" + LocationId + "')";
+                statement.executeUpdate(DirectorQuery);
+                ResultSet resultDirectorSet = statement.executeQuery("select director_id from directors");
+                resultDirectorSet.afterLast();
+                resultDirectorSet.previous();
+                String DirectorId = resultDirectorSet.getString(1);
+                String MovieQuery = "insert into movies(movie_name, movie_coords, date_of_creation, oscars, length, movie_genre, movie_rating, director, movie_key, usernames) values ('"
+                        + movie.getName() + "', '" + CoordinatesId + "', '" + movie.getCreationDate() + "', " + movie.getOscars() + ", " + movie.getLength() + ", '" + movie.getGenre().toString() + "', '"
+                        + movie.getMpaaRating().toString() + "', '" + DirectorId + "', '" + parameter + "', '" + user + "')";
+                statement.executeUpdate(MovieQuery);
 
-            MovieCollection movieCollection = new MovieCollection();
-            movieCollection.getMovies().put(parameter, movie);
-            resultCoordinatesSet.close();
-            resultLocationSet.close();
-            resultDirectorSet.close();
+                MovieCollection movieCollection = new MovieCollection();
+                movieCollection.getMovies().put(parameter, movie);
+                resultCoordinatesSet.close();
+                resultLocationSet.close();
+                resultDirectorSet.close();
+                return "В базу данных успешно добавлен фильм " + movie.getName();
+            } else {
+                return "Не удалось добавить фильм. Фильм по заданному ключу " + parameter + " уже существует";
+            }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
+            return "При добавлении фильма произошла ошибка";
         }
-
-        return "В базу данных успешно добавлен фильм " + movie.getName();
     }
 }
