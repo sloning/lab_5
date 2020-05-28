4a47976d13ebf5dd6cc47cdf06032114d3a755f8
takov-abay <61031325+takov-abay@users.noreply.github.com>
2020-05-28 17:43:07 +0300

    появилась автозагрузка с бд

--- server/src/main/java/DB/DBLoader.java
+++ server/src/main/java/DB/DBLoader.java
@@ -4,14 +4,23 @@ import Collection.MovieCollection;
 import movie.Movie;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DBLoader {
     public DBLoader(){
         try {
-            Statement statement = DBWorker.getConnection().createStatement();
+            Statement statement = DBWorker.getConnection().createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
             MovieCollection movieCollection = new MovieCollection();
+            List<Movie> movies = new ArrayList<>();
+            ResultSet resultSet = statement.executeQuery("select * from movies");
+
+            int i = -1;
+            while (resultSet.last()) {
+                i = resultSet.getRow();
+            }
             ResultSet resultMoviesSet = statement.executeQuery("select * from movies");
-            while (resultMoviesSet.next()) {
+            while (i > 0) {
                 Movie movie = new Movie();
                 String key = resultMoviesSet.getString("movie_key");
                 movie.setName(resultMoviesSet.getString("movie_name"));
@@ -22,30 +31,46 @@ public class DBLoader {
                 movie.setCreationDate(resultMoviesSet.getDate("date_of_creation"));
                 String directorId = resultMoviesSet.getString("director");
                 ResultSet resultCoordsSet = statement.executeQuery("select * from coords where coords_id = '" + resultMoviesSet.getString("movie_coords") + "'");
+                int x = 0;
+                float y = 0;
                 while (resultCoordsSet.next()) {
-                    movie.setCoordinates(resultCoordsSet.getInt("x"), resultCoordsSet.getFloat("y"));
+                    x = resultCoordsSet.getInt("x");
+                    y = resultCoordsSet.getFloat("y");
                 }
+                movie.setCoordinates(x, y);
                 ResultSet resultDirectorSet = statement.executeQuery("select * from directors where director_id = '" + directorId + "'");
+                String locationId = null;
+                String directorName = null;
+                double directorHeight = 0;
+                float directorWeight = 0;
+
                 while (resultDirectorSet.next()) {
-                    String locationId = resultDirectorSet.getString("location");
-                    String directorName = resultDirectorSet.getString("director_name");
-                    Double directorHeight = resultDirectorSet.getDouble("height");
-                    Float directorWeight = resultDirectorSet.getFloat("weight");
-                    ResultSet resultLocationSet = statement.executeQuery("select * from locations where location_id = '" + locationId + "'");
-                    while (resultLocationSet.next()) {
-                        String locationName = resultLocationSet.getString("location_name");
-                        int locationX = resultLocationSet.getInt("x");
-                        long locationY = resultLocationSet.getLong("y");
-                        int locationZ = resultLocationSet.getInt("z");
-                        movie.setDirectorWithLocation(directorName, directorHeight, directorWeight, locationName, locationX, locationY, locationZ);
-                    }
-                    resultLocationSet.close();
+                    locationId = resultDirectorSet.getString("location");
+                    directorName = resultDirectorSet.getString("director_name");
+                    directorHeight = resultDirectorSet.getDouble("height");
+                    directorWeight = resultDirectorSet.getFloat("weight");
                 }
-                resultDirectorSet.close();
-                resultCoordsSet.close();
+
+                ResultSet resultLocationSet = statement.executeQuery("select * from locations where location_id = '" + locationId + "'");
+
+                String locationName = null;
+                int locationX = 0;
+                long locationY = 0;
+                int locationZ = 0;
+
+                while (resultLocationSet.next()) {
+                    locationName = resultLocationSet.getString("location_name");
+                    locationX = resultLocationSet.getInt("x");
+                    locationY = resultLocationSet.getLong("y");
+                    locationZ = resultLocationSet.getInt("z");
+                }
+                movie.setDirectorWithLocation(directorName, directorHeight, directorWeight, locationName, locationX, locationY, locationZ);
+
                 movieCollection.putMovie(key, movie);
+                i--;
+                resultMoviesSet.next();
             }
-            resultMoviesSet.close();
+        resultMoviesSet.close();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
--- server/src/main/java/commands/Clear.java
+++ server/src/main/java/commands/Clear.java
@@ -1,9 +1,13 @@
 package commands;
 
 import Collection.MovieCollection;
+import DB.DBWorker;
 import movie.Movie;
 
-import java.util.Map;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.*;
 
 /**
  * Removes all movies from collection
@@ -14,7 +18,7 @@ public class Clear implements ICommand {
     /**
      * @param name = name of command
      */
-    private final String name;
+    private String name;
 
     public Clear() {
         this.name = "clear";
@@ -35,17 +39,25 @@ public class Clear implements ICommand {
      * @param parameter1 ignore this
      */
     @Override
-    public String Do(String parameter1, Movie nemovie, String user) {
+    public String Do(String parameter1, Movie movie, String user) {
         MovieCollection movieCollection = new MovieCollection();
-        for (Map.Entry<String, Movie> elementOfMap : movieCollection.getMap().entrySet()) {
-            Movie movie = elementOfMap.getValue();
-            String movieKey = elementOfMap.getKey();
+        for(Movie movieC : movieCollection.getMovies().values()) {
             int i = 0;
-            if (user.equals(movie.getUser())) {
+            if (user.equals(movieC.getUser()))  {
                 i++;
-                movieCollection.getMovies().remove(movieKey);
+                Collection<String> collection= movieCollection.getMovies().keySet();
+                String key = null;
+                for (String key1 : collection) {
+                    Movie obj = movieCollection.getMovies().get(key1);
+                    if (key1 != null) {
+                        if (movie.equals(obj)) {
+                            key = key1;//
+                        }
+                    }
+                }
+                movieCollection.getMovies().remove(key);
             }
-            if (i > 0) {
+            if (i>0) {
                 return "Все фильмы, принадлежащие вам успешно удалены из коллекции (в количестве - " + i + ")";
             }
         }
